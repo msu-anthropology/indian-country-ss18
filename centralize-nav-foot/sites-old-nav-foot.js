c4673db7245f46bed75b07ceba3927c632def6c8
Brian Geyer <geyerbri@msu.edu>
2017-11-29 01:18:32 -0500

    lots of changes (see discussion)
    centralize nav and foot
    add favicon
    delete extra files
    delete old student work
    move examples
    move/rename csv

---
+++ centralize-nav-foot/sites-old-nav-foot.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    $("#central-nav").load("../centralize-nav-foot/sites-old-navbar.html");
+	$("#central-foot").load("../centralize-nav-foot/sites-old-footer.html");
+});
